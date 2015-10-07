4ddeddac95c2f542fdc0b5a9a404ba166ebc20f6
dataems <dataems@163.com>
2015-10-07 20:26:51 +0800

    change fibo

--- dataspy/hello.py
+++ dataspy/hello.py
@@ -1,4 +1,3 @@
 from maths import fibo
 import math
-print('The value of PI is approximately %5.3f.'%math.pi)
 fibo.fib(1000)
--- dataspy/maths/fibo.py
+++ dataspy/maths/fibo.py
@@ -3,7 +3,7 @@
 def fib(n):    # write Fibonacci series up to n
     a, b = 0, 1
     while b < n:
-        print(b, end=' ')
+        print(b)
         a, b = b, a+b
     print()
 
